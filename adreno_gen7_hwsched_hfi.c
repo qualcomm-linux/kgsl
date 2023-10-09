c92706b1f39b3999a2a82be55cdbb0d1fb272517
Mohammed Mirza Mandayappurath Manzoor <quic_mmandaya@quicinc.com>
2023-10-09 11:26:10 -0700

    kgsl: hwsched: Force coldboot after a perfcounter release
    GMU requests for perfcounters during init process. The responses are
    recorded in scratch for warmboot execution. When the perfcounters are
    released, we need to force a coldboot to prevent GMU from reading the
    scratch and marking the counters as active.
    
    Change-Id: Ie0669b9ea1e4f3fba2226196ae43603c3ffbdf8f
    Signed-off-by: Mohammed Mirza Mandayappurath Manzoor <quic_mmandaya@quicinc.com>

--- adreno_gen7_hwsched_hfi.c
+++ adreno_gen7_hwsched_hfi.c
@@ -1278,6 +1278,8 @@ void gen7_hwsched_process_msgq(struct adreno_device *adreno_dev)
 
 			adreno_perfcounter_put(adreno_dev,
 				cmd->group_id, cmd->countable, PERFCOUNTER_FLAG_KERNEL);
+
+			adreno_mark_for_coldboot(adreno_dev);
 			}
 			break;
 		}
