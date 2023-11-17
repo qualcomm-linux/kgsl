1b41ec070a6a20f1ed2da83f106121c8b69eb5f9
Hareesh Gundu <quic_hareeshg@quicinc.com>
2023-11-16 18:59:58 -0800

    kgsl: gen8: Enable LPAC and AQE features for gen8_0_0
    Enable LPAC and AQE feature for gen8_0_0 to support
    additional graphics functionality.
    
    Change-Id: Idcfba1877106f1594b71c2b6bc1c6147646e1865
    Signed-off-by: Hareesh Gundu <quic_hareeshg@quicinc.com>

--- adreno-gpulist.h
+++ adreno-gpulist.h
@@ -2633,7 +2633,7 @@ static const struct adreno_gen8_core adreno_gpu_core_gen8_0_0 = {
 				  UINT_MAX, UINT_MAX, UINT_MAX, ANY_ID),
 		.compatible = "qcom,adreno-gpu-gen8-0-0",
 		.features = ADRENO_APRIV | ADRENO_IOCOHERENT |
-			ADRENO_CONTENT_PROTECTION,
+			ADRENO_CONTENT_PROTECTION | ADRENO_LPAC | ADRENO_AQE,
 		.gpudev = &adreno_gen8_hwsched_gpudev.base,
 		.perfcounters = &adreno_gen8_perfcounters,
 		.uche_gmem_alignment = SZ_16M,
