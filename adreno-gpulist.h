79df41556aba2f47bcd4a31937d06ad3b7d8f0b5
Urvashi Agrawal <quic_urvaagra@quicinc.com>
2024-02-01 16:19:13 -0800

    kgsl: gen8: Enable L0 preemption for gen8_0_0
    Enable L0 preemption for gen8_0_0 GPU.
    
    Change-Id: Ie1b628b46f7db6d0b8a6b62376263d0d7201a88e
    Signed-off-by: Urvashi Agrawal <quic_urvaagra@quicinc.com>

--- adreno-gpulist.h
+++ adreno-gpulist.h
@@ -2712,7 +2712,7 @@ static const struct adreno_gen8_core adreno_gpu_core_gen8_0_0 = {
 		.features = ADRENO_APRIV | ADRENO_IOCOHERENT |
 			ADRENO_CONTENT_PROTECTION | ADRENO_LPAC | ADRENO_AQE |
 			ADRENO_GMU_WARMBOOT | ADRENO_L3_VOTE | ADRENO_BCL |
-			ADRENO_IFPC | ADRENO_HW_FENCE,
+			ADRENO_IFPC | ADRENO_HW_FENCE | ADRENO_PREEMPTION,
 		.gpudev = &adreno_gen8_hwsched_gpudev.base,
 		.perfcounters = &adreno_gen8_perfcounters,
 		.uche_gmem_alignment = SZ_64M,
