cc2a4d0125d75ac855cfc12c17cd20d5cabda318
Kamal Agrawal <quic_kamaagra@quicinc.com>
2024-02-28 11:12:42 +0530

    kgsl: Clean up snapshot header file
    Clean up the snapshot header file by removing macros
    and structures that are no longer in use.
    
    Change-Id: Iaaf99428e465d0cd886f2ae002433b49a66184a2
    Signed-off-by: Kamal Agrawal <quic_kamaagra@quicinc.com>

--- kgsl_snapshot.h
+++ kgsl_snapshot.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2023, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2024, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef _KGSL_SNAPSHOT_H_
@@ -39,18 +39,13 @@ struct kgsl_snapshot_section_header {
 #define KGSL_SNAPSHOT_SECTION_OS           0x0101
 #define KGSL_SNAPSHOT_SECTION_REGS         0x0201
 #define KGSL_SNAPSHOT_SECTION_REGS_V2      0x0202
-#define KGSL_SNAPSHOT_SECTION_RB           0x0301
 #define KGSL_SNAPSHOT_SECTION_RB_V2        0x0302
-#define KGSL_SNAPSHOT_SECTION_IB           0x0401
 #define KGSL_SNAPSHOT_SECTION_IB_V2        0x0402
 #define KGSL_SNAPSHOT_SECTION_INDEXED_REGS 0x0501
 #define KGSL_SNAPSHOT_SECTION_INDEXED_REGS_V2 0x0502
-#define KGSL_SNAPSHOT_SECTION_ISTORE       0x0801
 #define KGSL_SNAPSHOT_SECTION_DEBUG        0x0901
 #define KGSL_SNAPSHOT_SECTION_DEBUGBUS     0x0A01
-#define KGSL_SNAPSHOT_SECTION_GPU_OBJECT   0x0B01
 #define KGSL_SNAPSHOT_SECTION_GPU_OBJECT_V2 0x0B02
-#define KGSL_SNAPSHOT_SECTION_MEMLIST      0x0E01
 #define KGSL_SNAPSHOT_SECTION_MEMLIST_V2   0x0E02
 #define KGSL_SNAPSHOT_SECTION_SHADER       0x1201
 #define KGSL_SNAPSHOT_SECTION_SHADER_V2    0x1202
@@ -58,7 +53,6 @@ struct kgsl_snapshot_section_header {
 #define KGSL_SNAPSHOT_SECTION_MVC          0x1501
 #define KGSL_SNAPSHOT_SECTION_MVC_V2       0x1502
 #define KGSL_SNAPSHOT_SECTION_MVC_V3       0x1503
-#define KGSL_SNAPSHOT_SECTION_GMU          0x1601
 #define KGSL_SNAPSHOT_SECTION_GMU_MEMORY   0x1701
 #define KGSL_SNAPSHOT_SECTION_SIDE_DEBUGBUS 0x1801
 #define KGSL_SNAPSHOT_SECTION_TRACE_BUFFER 0x1901
@@ -67,46 +61,9 @@ struct kgsl_snapshot_section_header {
 #define KGSL_SNAPSHOT_SECTION_END          0xFFFF
 
 /* OS sub-section header */
-#define KGSL_SNAPSHOT_OS_LINUX             0x0001
-#define KGSL_SNAPSHOT_OS_LINUX_V3          0x00000202
 #define KGSL_SNAPSHOT_OS_LINUX_V4          0x00000203
 
 /* Linux OS specific information */
-struct kgsl_snapshot_linux {
-	int osid;                   /* subsection OS identifier */
-	int state;		    /* 1 if the thread is running, 0 for hung */
-	__u32 seconds;		    /* Unix timestamp for the snapshot */
-	__u32 power_flags;            /* Current power flags */
-	__u32 power_level;            /* Current power level */
-	__u32 power_interval_timeout; /* Power interval timeout */
-	__u32 grpclk;                 /* Current GP clock value */
-	__u32 busclk;		    /* Current busclk value */
-	__u32 ptbase;		    /* Current ptbase */
-	__u32 pid;		    /* PID of the process that owns the PT */
-	__u32 current_context;	    /* ID of the current context */
-	__u32 ctxtcount;	    /* Number of contexts appended to section */
-	unsigned char release[32];  /* kernel release */
-	unsigned char version[32];  /* kernel version */
-	unsigned char comm[16];	    /* Name of the process that owns the PT */
-} __packed;
-
-struct kgsl_snapshot_linux_v2 {
-	int osid;                   /* subsection OS identifier */
-	__u32 seconds;		    /* Unix timestamp for the snapshot */
-	__u32 power_flags;            /* Current power flags */
-	__u32 power_level;            /* Current power level */
-	__u32 power_interval_timeout; /* Power interval timeout */
-	__u32 grpclk;                 /* Current GP clock value */
-	__u32 busclk;		    /* Current busclk value */
-	__u64 ptbase;		    /* Current ptbase */
-	__u32 pid;		    /* PID of the process that owns the PT */
-	__u32 current_context;	    /* ID of the current context */
-	__u32 ctxtcount;	    /* Number of contexts appended to section */
-	unsigned char release[32];  /* kernel release */
-	unsigned char version[32];  /* kernel version */
-	unsigned char comm[16];	    /* Name of the process that owns the PT */
-} __packed;
-
 struct kgsl_snapshot_linux_v4 {
 	int osid;		/* subsection OS identifier */
 	__u32 seconds;		/* Unix timestamp for the snapshot */
@@ -133,31 +90,14 @@ struct kgsl_snapshot_linux_v4 {
  * These are appended one after another in the OS section below
  * the header above
  */
-
-struct kgsl_snapshot_linux_context {
-	__u32 id;			/* The context ID */
-	__u32 timestamp_queued;		/* The last queued timestamp */
-	__u32 timestamp_retired;	/* The last timestamp retired by HW */
-};
-
 struct kgsl_snapshot_linux_context_v2 {
 	__u32 id;			/* The context ID */
 	__u32 timestamp_queued;		/* The last queued timestamp */
 	__u32 timestamp_consumed;	/* The last timestamp consumed by HW */
 	__u32 timestamp_retired;	/* The last timestamp retired by HW */
 };
-/* Ringbuffer sub-section header */
-struct kgsl_snapshot_rb {
-	int start;  /* dword at the start of the dump */
-	int end;    /* dword at the end of the dump */
-	int rbsize; /* Size (in dwords) of the ringbuffer */
-	int wptr;   /* Current index of the CPU write pointer */
-	int rptr;   /* Current index of the GPU read pointer */
-	int count;  /* Number of dwords in the dump */
-	__u32 timestamp_queued; /* The last queued timestamp */
-	__u32 timestamp_retired; /* The last timestamp retired by HW */
-} __packed;
 
+/* Ringbuffer sub-section header */
 struct kgsl_snapshot_rb_v2 {
 	int start;  /* dword at the start of the dump */
 	int end;    /* dword at the end of the dump */
@@ -171,18 +111,6 @@ struct kgsl_snapshot_rb_v2 {
 	__u32 id; /* Ringbuffer identifier */
 } __packed;
 
-
-/* Replay or Memory list section, both sections have same header */
-struct kgsl_snapshot_replay_mem_list {
-	/*
-	 * Number of IBs to replay for replay section or
-	 * number of memory list entries for mem list section
-	 */
-	int num_entries;
-	/* Pagetable base to which the replay IBs or memory entries belong */
-	__u32 ptbase;
-} __packed;
-
 /* Replay or Memory list section, both sections have same header */
 struct kgsl_snapshot_mem_list_v2 {
 	/*
@@ -192,14 +120,6 @@ struct kgsl_snapshot_mem_list_v2 {
 	int num_entries;
 	/* Pagetable base to which the replay IBs or memory entries belong */
 	__u64 ptbase;
-} __packed;
-
-
-/* Indirect buffer sub-section header */
-struct kgsl_snapshot_ib {
-	__u32 gpuaddr; /* GPU address of the the IB */
-	__u32 ptbase;  /* Base for the pagetable the GPU address is valid in */
-	int size;    /* Size of the IB */
 } __packed;
 
 /* Indirect buffer sub-section header (v2) */
@@ -275,22 +195,9 @@ struct kgsl_snapshot_mvc_regs_v3 {
 	u32 usptp_id;
 } __packed;
 
-/* Istore sub-section header */
-struct kgsl_snapshot_istore {
-	int count;   /* Number of instructions in the istore */
-} __packed;
-
 /* Debug data sub-section header */
 
-/* A2XX debug sections */
-#define SNAPSHOT_DEBUG_SX         1
-#define SNAPSHOT_DEBUG_CP         2
-#define SNAPSHOT_DEBUG_SQ         3
-#define SNAPSHOT_DEBUG_SQTHREAD   4
-#define SNAPSHOT_DEBUG_MIU        5
-
-/* A3XX debug sections */
-#define SNAPSHOT_DEBUG_VPC_MEMORY 6
+/* A5XX debug sections */
 #define SNAPSHOT_DEBUG_CP_MEQ     7
 #define SNAPSHOT_DEBUG_CP_PM4_RAM 8
 #define SNAPSHOT_DEBUG_CP_PFP_RAM 9
@@ -384,13 +291,6 @@ struct kgsl_snapshot_trace_buffer {
 #define SNAPSHOT_GPU_OBJECT_GENERIC 3
 #define SNAPSHOT_GPU_OBJECT_DRAW    4
 #define SNAPSHOT_GPU_OBJECT_GLOBAL  5
-
-struct kgsl_snapshot_gpu_object {
-	int type;      /* Type of GPU object */
-	__u32 gpuaddr; /* GPU address of the the object */
-	__u32 ptbase;  /* Base for the pagetable the GPU address is valid in */
-	int size;    /* Size of the object (in dwords) */
-};
 
 struct kgsl_snapshot_gpu_object_v2 {
 	int type;      /* Type of GPU object */
