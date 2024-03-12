6f3afc1a3bd0792bdf7e23bc1cdc652418a879c3
Kamal Agrawal <quic_kamaagra@quicinc.com>
2024-03-12 20:47:15 +0530

    kgsl: hwsched: Avoid unclocked access in interrupt handler
    KGSL handles few non-fatal interrupts like AHB error, TSB
    write error. Consider below sequence of execution:
    1. Non fatal interrupt is generated by GPU
    2. GPU Workload finishes
    3. GMU removes its vote on GX
    4. Interrupt handling in CPU - Access GPU register in
    interrupt handler (gen*_err_callback). This can result
    in device crash due to unclocked access.
    
    Fix this by reverting commit e8c1784fd392 ("kgsl: hwsched:
    Don’t put keepalive vote in irq_handler").
    
    Change-Id: Id4694d13b663a1d617bd1d28f53a3e2252c38523
    Signed-off-by: Kamal Agrawal <quic_kamaagra@quicinc.com>

--- adreno_gen7.c
+++ adreno_gen7.c
@@ -1590,28 +1590,6 @@ static int gen7_irq_poll_fence(struct adreno_device *adreno_dev)
 	return 0;
 }
 
-static irqreturn_t gen7_hwsched_irq_handler(struct adreno_device *adreno_dev)
-{
-	struct kgsl_device *device = KGSL_DEVICE(adreno_dev);
-	irqreturn_t ret = IRQ_NONE;
-	u32 status;
-
-	if (gen7_irq_poll_fence(adreno_dev)) {
-		adreno_hwsched_fault(adreno_dev, ADRENO_GMU_FAULT);
-		return ret;
-	}
-
-	kgsl_regread(device, GEN7_RBBM_INT_0_STATUS, &status);
-
-	kgsl_regwrite(device, GEN7_RBBM_INT_CLEAR_CMD, status);
-
-	ret = adreno_irq_callbacks(adreno_dev, gen7_irq_funcs, status);
-
-	trace_kgsl_gen7_irq_status(adreno_dev, status);
-
-	return ret;
-}
-
 static irqreturn_t gen7_irq_handler(struct adreno_device *adreno_dev)
 {
 	struct kgsl_device *device = KGSL_DEVICE(adreno_dev);
@@ -2214,7 +2192,7 @@ const struct gen7_gpudev adreno_gen7_9_0_hwsched_gpudev = {
 		.reg_offsets = gen7_register_offsets,
 		.probe = gen7_hwsched_probe,
 		.snapshot = gen7_hwsched_snapshot,
-		.irq_handler = gen7_hwsched_irq_handler,
+		.irq_handler = gen7_irq_handler,
 		.iommu_fault_block = gen7_iommu_fault_block,
 		.context_detach = gen7_hwsched_context_detach,
 		.read_alwayson = gen7_9_0_read_alwayson,
@@ -2243,7 +2221,7 @@ const struct gen7_gpudev adreno_gen7_hwsched_gpudev = {
 		.reg_offsets = gen7_register_offsets,
 		.probe = gen7_hwsched_probe,
 		.snapshot = gen7_hwsched_snapshot,
-		.irq_handler = gen7_hwsched_irq_handler,
+		.irq_handler = gen7_irq_handler,
 		.iommu_fault_block = gen7_iommu_fault_block,
 		.context_detach = gen7_hwsched_context_detach,
 		.read_alwayson = gen7_read_alwayson,
