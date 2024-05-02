2946351191e40c9f07f3045a2f804290f4db3366
Kamal Agrawal <quic_kamaagra@quicinc.com>
2024-05-02 10:27:18 +0530

    kgsl: gen8: Enable ACD on gen8_4_0 GPU
    Adaptive Clock Distribution feature helps mitigate peak
    current and voltage droops.
    
    Change-Id: I21429b44c4a0bbf80de511f4b40eec5f1c642617
    Signed-off-by: Kamal Agrawal <quic_kamaagra@quicinc.com>

--- adreno-gpulist.h
+++ adreno-gpulist.h
@@ -2893,7 +2893,7 @@ static const struct adreno_gen8_core adreno_gpu_core_gen8_4_0 = {
 		.features = ADRENO_APRIV | ADRENO_IOCOHERENT |
 			ADRENO_CONTENT_PROTECTION | ADRENO_LPAC | ADRENO_AQE |
 			ADRENO_GMU_WARMBOOT | ADRENO_L3_VOTE | ADRENO_IFPC |
-			ADRENO_BCL,
+			ADRENO_BCL | ADRENO_ACD,
 		.gpudev = &adreno_gen8_hwsched_gpudev.base,
 		.perfcounters = &adreno_gen8_perfcounters,
 		.uche_gmem_alignment = SZ_64M,
