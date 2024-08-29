b671594f618a0b033ca5dc9c5ab885c495349558
Kamal Agrawal <quic_kamaagra@quicinc.com>
2024-08-29 21:17:40 +0530

    kgsl: Introduce dedicated mutex for device open and close operations
    KGSL device mutex is currently heavily contended, leading to potential
    performance and stability bottlenecks. In the existing design, device
    mutex is required during the first open and last close operations.
    However, for intermediate open and close operations, device mutex can be
    bypassed. Therefore, introduce a new mutex to specifically synchronize
    device open and close operations. This will reduce the contention on
    primary device mutex.
    
    Change-Id: Iac9cbb3f83df4b308061f21efdc41a1e95be9220
    Signed-off-by: Kamal Agrawal <quic_kamaagra@quicinc.com>

--- kgsl.c
+++ kgsl.c
@@ -1366,9 +1366,12 @@ static int kgsl_close_device(struct kgsl_device *device)
 {
 	int result = 0;
 
-	mutex_lock(&device->mutex);
-	if (device->open_count == 1)
+	mutex_lock(&device->file_mutex);
+	if (device->open_count == 1) {
+		mutex_lock(&device->mutex);
 		result = device->ftbl->last_close(device);
+		mutex_unlock(&device->mutex);
+	}
 
 	/*
 	 * We must decrement the open_count after last_close() has finished.
@@ -1381,7 +1384,7 @@ static int kgsl_close_device(struct kgsl_device *device)
 	 * last_close().
 	 */
 	device->open_count--;
-	mutex_unlock(&device->mutex);
+	mutex_unlock(&device->file_mutex);
 	return result;
 
 }
@@ -1446,15 +1449,18 @@ static int kgsl_open_device(struct kgsl_device *device)
 {
 	int result = 0;
 
-	mutex_lock(&device->mutex);
+	mutex_lock(&device->file_mutex);
 	if (device->open_count == 0) {
+		mutex_lock(&device->mutex);
 		result = device->ftbl->first_open(device);
+		mutex_unlock(&device->mutex);
+
 		if (result)
 			goto out;
 	}
 	device->open_count++;
 out:
-	mutex_unlock(&device->mutex);
+	mutex_unlock(&device->file_mutex);
 	return result;
 }
 
