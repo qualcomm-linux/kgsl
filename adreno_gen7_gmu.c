1bac02f33af136433e4db8773edc9387c87209c4
Kamal Agrawal <quic_kamaagra@quicinc.com>
2024-09-05 14:00:28 +0530

    kgsl: gmu: Remove redundant idle timer start calls
    KGSL idle timer is initiated once the active count reaches zero.
    Therefore, there is no need to start the idle timer when the
    active count is non-zero. Similarly, when the specified time is
    after jiffies, the idle timer callback will be triggered once the
    timer expires. Consequently, remove these redundant idle timer
    start calls.
    
    Change-Id: I99fa5be47295a6df3e14b312cd80888f3fb3f1c2
    Signed-off-by: Kamal Agrawal <quic_kamaagra@quicinc.com>

--- adreno_gen7_gmu.c
+++ adreno_gen7_gmu.c
@@ -3158,7 +3158,6 @@ static void gmu_idle_check(struct work_struct *work)
 
 	if (atomic_read(&device->active_cnt) || time_is_after_jiffies(device->idle_jiffies)) {
 		kgsl_pwrscale_update(device);
-		kgsl_start_idle_timer(device);
 		goto done;
 	}
 
