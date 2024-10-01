1621dd6b81edd893e07e7ea5835efc07f4b0c51f
NISARG SHETH <quic_nsheth@quicinc.com>
2024-10-01 15:51:08 +0530

    kgsl: gen8: Do not program UCHE_CACHE_WAYS register
    Do not program UCHE_CACHE_WAYS register as per the latest recommendation.
    
    Change-Id: I3aa925cdd67150d2a707462c303f699de8ce9e84
    Signed-off-by: NISARG SHETH <quic_nsheth@quicinc.com>

--- adreno-gpulist.h
+++ adreno-gpulist.h
@@ -2825,7 +2825,6 @@ static const struct gen8_nonctxt_regs gen8_0_0_nonctxt_regs[] = {
 	{ GEN8_UCHE_CCHE_MODE_CNTL, 0x00001000, BIT(PIPE_NONE) },
 	/* Limit gmem number of ways for GMEM requests in each set */
 	{ GEN8_UCHE_CCHE_CACHE_WAYS, 0x00000800, BIT(PIPE_NONE)},
-	{ GEN8_UCHE_CACHE_WAYS, 0x00080000, BIT(PIPE_NONE) },
 	/* Disable write slow pointer in data phase queue */
 	{ GEN8_UCHE_HW_DBG_CNTL, BIT(8), BIT(PIPE_NONE) },
 	/* Configure UCHE to CCU switchthreshold timeout cycles */
@@ -3030,7 +3029,6 @@ static const struct gen8_nonctxt_regs gen8_4_0_nonctxt_regs[] = {
 	{ GEN8_UCHE_CCHE_MODE_CNTL, 0x00001000, BIT(PIPE_NONE) },
 	/* Limit gmem number of ways for GMEM requests in each set */
 	{ GEN8_UCHE_CCHE_CACHE_WAYS, 0x00000800, BIT(PIPE_NONE)},
-	{ GEN8_UCHE_CACHE_WAYS, 0x00080000, BIT(PIPE_NONE) },
 	/* Disable write slow pointer in data phase queue */
 	{ GEN8_UCHE_HW_DBG_CNTL, BIT(8), BIT(PIPE_NONE) },
 	/* Configure UCHE to CCU switchthreshold timeout cycles */
@@ -3131,7 +3129,6 @@ static const struct gen8_nonctxt_regs gen8_3_0_nonctxt_regs[] = {
 	{ GEN8_UCHE_CCHE_CACHE_WAYS, 0x00000800, BIT(PIPE_NONE) },
 	/* Disable write slow pointer in data phase queue */
 	{ GEN8_UCHE_HW_DBG_CNTL, BIT(8), BIT(PIPE_NONE) },
-	{ GEN8_UCHE_CACHE_WAYS, 0x00080000, BIT(PIPE_NONE) },
 	{ GEN8_UCHE_VARB_IDLE_TIMEOUT, 0x00000020, BIT(PIPE_NONE) },
 	{ GEN8_VFD_DBG_ECO_CNTL, 0x00008000, BIT(PIPE_BV) | BIT(PIPE_BR) },
 	{ GEN8_VFD_CB_BV_THRESHOLD, 0x00500050, BIT(PIPE_BV) | BIT(PIPE_BR) },
