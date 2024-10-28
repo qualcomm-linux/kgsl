c3e8018cb57bcf8a380e37d5dcdb04017ac2a36b
Hareesh Gundu <quic_hareeshg@quicinc.com>
2024-10-28 16:06:51 -0700

    kgsl: gen8: Enable LPAC, AQE and GMU warmboot features for gen8_2_0
    Enable following features to support respective functionalities
    	1) ADRENO_LPAC
    	2) ADRENO_AQE
    	3) ADRENO_GMU_WARMBOOT
    
    Change-Id: I4ec8fe35ec4cf2680d96ee8ebb67b106d965708a
    Signed-off-by: Hareesh Gundu <quic_hareeshg@quicinc.com>

--- adreno-gpulist.h
+++ adreno-gpulist.h
@@ -3123,7 +3123,8 @@ static const struct adreno_gen8_core adreno_gpu_core_gen8_2_0 = {
 				  UINT_MAX, UINT_MAX, UINT_MAX, ANY_ID),
 		.compatible = "qcom,adreno-gpu-gen8-2-0",
 		.features = ADRENO_APRIV | ADRENO_IOCOHERENT |
-			ADRENO_PREEMPTION,
+			ADRENO_PREEMPTION | ADRENO_LPAC | ADRENO_AQE |
+			ADRENO_GMU_WARMBOOT,
 		.gpudev = &adreno_gen8_hwsched_gpudev.base,
 		.perfcounters = &adreno_gen8_perfcounters,
 		.uche_gmem_alignment = SZ_64M,
