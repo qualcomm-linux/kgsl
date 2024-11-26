36a1759bf8a7bd70975ffcf9209af5e590c5ebcf
SIVA MULLATI <quic_smullati@quicinc.com>
2024-11-26 22:19:43 +0530

    msm: kgsl: Enable IFPC feature for gen_8_6_0
    Enable IFPC feature for GPU power saving.
    
    Change-Id: I5d80b2aa9d12b1770d92801ec9167e8083efca25
    Signed-off-by: SIVA MULLATI <quic_smullati@quicinc.com>

--- adreno-gpulist.h
+++ adreno-gpulist.h
@@ -3204,7 +3204,7 @@ static const struct adreno_gen8_core adreno_gpu_core_gen8_6_0 = {
 		.compatible = "qcom,adreno-gpu-gen8-6-0",
 		.features = ADRENO_APRIV | ADRENO_IOCOHERENT |
 			ADRENO_CONTENT_PROTECTION | ADRENO_LPAC | ADRENO_AQE |
-			ADRENO_GMU_WARMBOOT | ADRENO_L3_VOTE,
+			ADRENO_GMU_WARMBOOT | ADRENO_L3_VOTE | ADRENO_IFPC,
 		.gpudev = &adreno_gen8_hwsched_gpudev.base,
 		.perfcounters = &adreno_gen8_perfcounters,
 		.uche_gmem_alignment = SZ_64M,
