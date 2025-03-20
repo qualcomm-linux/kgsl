0fff2b124aedf558de32352e06d6748ff2748a6d
Sanjay Yadav <quic_sanjyada@quicinc.com>
2025-03-20 18:49:47 +0530

    kgsl: hwsched: Set ADRENO_CONTEXT_FAULT flag upon GPU fault
    Currently, we do not check and set ADRENO_CONTEXT_FAULT after
    queuing the command, which results in IOCTL_KGSL_GPU_COMMAND
    not returning the appropriate error to the user.
    
    Add a check to ensure that if there is a gpu fault,
    it returns EPROTO error to userspace. This error indicates
    to userspace to start pulling the snapshot from the sysfs
    node (kgsl-3d0/snapshot) into the specified path.
    
    Change-Id: I75ac9eec87abd64e256dd1bc6e240eea04ba25f1
    Signed-off-by: Sanjay Yadav <quic_sanjyada@quicinc.com>

--- adreno_hwsched.c
+++ adreno_hwsched.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2024, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2025, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include "adreno.h"
@@ -1090,6 +1090,9 @@ static int adreno_hwsched_queue_cmds(struct kgsl_device_private *dev_priv,
 	} else
 		kmem_cache_free(jobs_cache, job);
 
+	if (test_and_clear_bit(ADRENO_CONTEXT_FAULT, &context->priv))
+		return -EPROTO;
+
 	return 0;
 }
 
@@ -1711,6 +1714,9 @@ static void adreno_hwsched_reset_and_snapshot_legacy(struct adreno_device *adren
 			drawobj = NULL;
 	}
 
+	if (drawobj && drawobj->context)
+		set_bit(ADRENO_CONTEXT_FAULT, &drawobj->context->priv);
+
 	adreno_gpufault_stats(adreno_dev, drawobj, NULL, fault);
 
 	if (!drawobj) {
@@ -1796,6 +1802,9 @@ static void adreno_hwsched_reset_and_snapshot(struct adreno_device *adreno_dev, 
 			drawobj = NULL;
 	}
 
+	if (drawobj && drawobj->context)
+		set_bit(ADRENO_CONTEXT_FAULT, &drawobj->context->priv);
+
 	do_fault_header(adreno_dev, drawobj, fault);
 
 	if (!obj_lpac && (fault & ADRENO_IOMMU_STALL_ON_PAGE_FAULT))
@@ -1821,6 +1830,9 @@ static void adreno_hwsched_reset_and_snapshot(struct adreno_device *adreno_dev, 
 		if (gpudev->lpac_fault_header)
 			gpudev->lpac_fault_header(adreno_dev, drawobj_lpac);
 	}
+
+	if (drawobj_lpac && drawobj_lpac->context)
+		set_bit(ADRENO_CONTEXT_FAULT, &drawobj_lpac->context->priv);
 
 	kgsl_device_snapshot(device, context, context_lpac, false);
 	adreno_gpufault_stats(adreno_dev, drawobj, drawobj_lpac, fault);
