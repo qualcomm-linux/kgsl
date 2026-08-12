aa2f808658e2ed12720fb2394769b72abe156e04
Veeresh Bagale <vbagale@qti.qualcomm.com>
2026-08-12 12:17:20 +0530

    kgsl: gen8: enable aqe in gen8_1_0
    Enable AQE in gen8_1_0 GPU.
    
    Change-Id: I725bfa1da69b69a5772f892abb7e0390a3b9a6a9
    Signed-off-by: Veeresh Bagale <vbagale@qti.qualcomm.com>

--- adreno-gpulist.h
+++ adreno-gpulist.h
@@ -3749,7 +3749,7 @@ static const struct adreno_gen8_core adreno_gpu_core_gen8_1_0 = {
 		DEFINE_ADRENO_REV(ADRENO_REV_GEN8_1_0,
 				  UINT_MAX, UINT_MAX, UINT_MAX, ANY_ID),
 		.features = ADRENO_APRIV | ADRENO_IOCOHERENT | ADRENO_CONTENT_PROTECTION |
-			ADRENO_SOFTFUSE | ADRENO_ACD | ADRENO_PREEMPTION,
+			ADRENO_SOFTFUSE | ADRENO_ACD | ADRENO_PREEMPTION | ADRENO_AQE,
 		.gpudev = &adreno_gen8_hwsched_gpudev.base,
 		.perfcounters = &adreno_gen8_perfcounters,
 		.uche_gmem_alignment = SZ_64M,
