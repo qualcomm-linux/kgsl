CodeLinaro-mirror/la_platform_vendor_qcom_opensource_graphics-kernel

kgsl: Fix TOCTOU races in IB command stream parser
The IB parser maps user-allocated GPU buffers into kernel VA via
kgsl_gpuaddr_to_vaddr() and iterates over the packet stream directly
from those shared physical pages. Userspace retains write access,
creating TOCTOU races where packet headers can change between
validation and use.

Change-Id: Ie32f77c48e883a24a63dc1c8c2f98abc6586b30f
Signed-off-by: Scott Bauer <sbauer@qti.qualcomm.com>

adreno_cp_parser.c
@@ -41,7 +41,7 @@ static int adreno_ib_find_objs(struct kgsl_device *device,
 				int ib_level);
 
 static int ib_parse_type7_set_draw_state(struct kgsl_device *device,
-	unsigned int *ptr,
+	unsigned int *ptr, int size,
 	struct kgsl_process_private *process,
 	struct adreno_ib_object_list *ib_obj_list);
 
@@ -223,6 +223,7 @@ static int ib_add_type0_entries(struct kgsl_device *device,
  */
 
 static int ib_parse_type7(struct kgsl_device *device, unsigned int *ptr,
+	int pktsize,
 	struct kgsl_process_private *process,
 	struct adreno_ib_object_list *ib_obj_list,
 	struct ib_parser_variables *ib_parse_vars)
@@ -231,19 +232,18 @@ static int ib_parse_type7(struct kgsl_device *device, unsigned int *ptr,
 
 	switch (opcode) {
 	case CP_SET_DRAW_STATE:
-		return ib_parse_type7_set_draw_state(device, ptr, process,
-					ib_obj_list);
+		return ib_parse_type7_set_draw_state(device, ptr, pktsize,
+					process, ib_obj_list);
 	}
 
 	return 0;
 }
 
 static int ib_parse_type7_set_draw_state(struct kgsl_device *device,
-	unsigned int *ptr,
+	unsigned int *ptr, int size,
 	struct kgsl_process_private *process,
 	struct adreno_ib_object_list *ib_obj_list)
 {
-	int size = type7_pkt_size(*ptr);
 	int i;
 	int ret = 0;
 	int flags;
@@ -436,10 +436,12 @@ static int adreno_ib_find_objs(struct kgsl_device *device,
 		goto done;
 
 	for (i = 0; rem > 0;) {
-		if (pkt_is_type4(src[i]))
-			pkt_size = type4_pkt_size(src[i]);
-		else if (pkt_is_type7(src[i]))
-			pkt_size = type7_pkt_size(src[i]);
+		unsigned int hdr = src[i];
+
+		if (pkt_is_type4(hdr))
+			pkt_size = type4_pkt_size(hdr);
+		else if (pkt_is_type7(hdr))
+			pkt_size = type7_pkt_size(hdr);
 
 		/*
 		 * If the packet isn't a type 1, type 3, type 4 or type 7 then
@@ -455,8 +457,8 @@ static int adreno_ib_find_objs(struct kgsl_device *device,
 		if (rem < curr_size)
 			break;
 
-		if (pkt_is_type7(src[i])) {
-			if (adreno_cmd_is_ib(adreno_dev, src[i])) {
+		if (pkt_is_type7(hdr)) {
+			if (adreno_cmd_is_ib(adreno_dev, hdr)) {
 				u64 size = src[i + 3];
 				u64 gpuaddribn = ((u64)(src[i + 2]) << 32) | src[i + 1];
 				s64 next_ibbase = get_ib_base(adreno_dev, ib_level + IB_LEVEL_1);
@@ -468,15 +470,9 @@ static int adreno_ib_find_objs(struct kgsl_device *device,
 				if (ret)
 					goto done;
 			} else {
-				ret = ib_parse_type7(device, &src[i], process,
-						ib_obj_list,
+				ret = ib_parse_type7(device, &src[i], pkt_size,
+						process, ib_obj_list,
 						&ib_parse_vars);
-				/*
-				 * If the parse function failed (probably
-				 * because of a bad decode) then bail out and
-				 * just capture the binary IB data
-				 */
-
 				if (ret)
 					goto done;
 			}

qualcomm-linux/kgsl

Signed-off-by: Gangabai Jagu Jigajambagi <gjigajam@qti.qualcomm.com>

adreno_cp_parser.c
@@ -41,7 +41,7 @@ static int adreno_ib_find_objs(struct kgsl_device *device,
 				int ib_level);
 
 static int ib_parse_type7_set_draw_state(struct kgsl_device *device,
-	unsigned int *ptr,
+	unsigned int *ptr, int size,
 	struct kgsl_process_private *process,
 	struct adreno_ib_object_list *ib_obj_list);
 
@@ -223,6 +223,7 @@ static int ib_add_type0_entries(struct kgsl_device *device,
  */
 
 static int ib_parse_type7(struct kgsl_device *device, unsigned int *ptr,
+	int pktsize,
 	struct kgsl_process_private *process,
 	struct adreno_ib_object_list *ib_obj_list,
 	struct ib_parser_variables *ib_parse_vars)
@@ -231,19 +232,18 @@ static int ib_parse_type7(struct kgsl_device *device, unsigned int *ptr,
 
 	switch (opcode) {
 	case CP_SET_DRAW_STATE:
-		return ib_parse_type7_set_draw_state(device, ptr, process,
-					ib_obj_list);
+		return ib_parse_type7_set_draw_state(device, ptr, pktsize,
+					process, ib_obj_list);
 	}
 
 	return 0;
 }
 
 static int ib_parse_type7_set_draw_state(struct kgsl_device *device,
-	unsigned int *ptr,
+	unsigned int *ptr, int size,
 	struct kgsl_process_private *process,
 	struct adreno_ib_object_list *ib_obj_list)
 {
-	int size = type7_pkt_size(*ptr);
 	int i;
 	int ret = 0;
 	int flags;
@@ -435,11 +435,12 @@ static int adreno_ib_find_objs(struct kgsl_device *device,
 
 	for (i = 0; rem > 0; rem--, i++) {
 		int pktsize;
+		unsigned int hdr = src[i];
 
-		if (pkt_is_type4(src[i]))
-			pktsize = type4_pkt_size(src[i]);
-		else if (pkt_is_type7(src[i]))
-			pktsize = type7_pkt_size(src[i]);
+		if (pkt_is_type4(hdr))
+			pktsize = type4_pkt_size(hdr);
+		else if (pkt_is_type7(hdr))
+			pktsize = type7_pkt_size(hdr);
 
 		/*
 		 * If the packet isn't a type 1, type 3, type 4 or type 7 then
@@ -452,8 +453,8 @@ static int adreno_ib_find_objs(struct kgsl_device *device,
 		if (rem < pktsize)
 			break;
 
-		if (pkt_is_type7(src[i])) {
-			if (adreno_cmd_is_ib(adreno_dev, src[i])) {
+		if (pkt_is_type7(hdr)) {
+			if (adreno_cmd_is_ib(adreno_dev, hdr)) {
 				u64 size = src[i + 3];
 				u64 gpuaddribn = ((u64)(src[i + 2]) << 32) | src[i + 1];
 				s64 next_ibbase = get_ib_base(adreno_dev, ib_level + IB_LEVEL_1);
@@ -465,15 +466,9 @@ static int adreno_ib_find_objs(struct kgsl_device *device,
 				if (ret)
 					goto done;
 			} else {
-				ret = ib_parse_type7(device, &src[i], process,
-						ib_obj_list,
+				ret = ib_parse_type7(device, &src[i], pktsize,
+						process, ib_obj_list,
 						&ib_parse_vars);
-				/*
-				 * If the parse function failed (probably
-				 * because of a bad decode) then bail out and
-				 * just capture the binary IB data
-				 */
-
 				if (ret)
 					goto done;
 			}