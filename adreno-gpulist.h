82172eed9d4170d0512e94abac2d62eda54447b3
Harsh Dhoke <hdhoke@qti.qualcomm.com>
2026-08-18 16:41:48 +0530

    kgsl: gen8: enable hwsched on gen8_1_0
    Enable hwsched on gen8_1_0.
    
    - Add read_speedbin to hwsched gpudev to preserve softfuse speed-bin
      gating.
    - Standard kernel does not support secure SIDs, but GMU FW always
      requests a secure preempt buffer regardless. Back it with a PAGE_SIZE
      non-secure dummy buffer to give CP a valid GPU VA to write to.
    - Fix qcom_iommu_get_asid_nr() stub to return 0 instead of -EINVAL.
    
    Change-Id: Ie18c622d7e77c00e1e8f4798f5f4a72c86dd1b85
    Signed-off-by: Harsh Dhoke <hdhoke@qti.qualcomm.com>

--- adreno-gpulist.h
+++ adreno-gpulist.h
@@ -3750,7 +3750,7 @@ static const struct adreno_gen8_core adreno_gpu_core_gen8_1_0 = {
 				  UINT_MAX, UINT_MAX, UINT_MAX, ANY_ID),
 		.features = ADRENO_APRIV | ADRENO_IOCOHERENT | ADRENO_CONTENT_PROTECTION |
 			ADRENO_SOFTFUSE | ADRENO_ACD | ADRENO_PREEMPTION,
-		.gpudev = &adreno_gen8_gmu_gpudev.base,
+		.gpudev = &adreno_gen8_hwsched_gpudev.base,
 		.perfcounters = &adreno_gen8_perfcounters,
 		.uche_gmem_alignment = SZ_64M,
 		.gmem_size = 21 * SZ_1M,
